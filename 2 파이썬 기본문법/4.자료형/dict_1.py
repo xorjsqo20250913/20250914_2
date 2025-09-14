96526d41f801e1dd15ba3c75ea254c09c595899e
xorjsqo20250913 <xorjsqo@gmail.com>
2025-09-14 15:52:11 +0900

    Python 자료형 예제 코드 추가: compare, dict, tuple 예제 및 가이드

---
+++ 2 파이썬 기본문법/4.자료형/dict_1.py
@@ -0,0 +1,16 @@
+# dictionary
+# key:value
+# key는 중복 불가, value는 중복 가능
+# [] () {2,3} { '이름':'홍길동', 'age':20}
+# 이름 이랑 age 는 key, 홍길동, 20은 value
+# key는 중복되지 않음
+# abc['이름']  abc.get('이름')
+# abc['age'] = 30
+# abc['취미'] = "축구"
+dict_1 = {} # set? dictionary? --> dictionary
+print(dict_1)
+dict_1['name'] = '홍길동'
+print(dict_1)
+dict_1['age'] = 20
+print(dict_1)
+
--- 2 파이썬 기본문법/4.자료형/tuple_1.py
+++ 2 파이썬 기본문법/4.자료형/tuple_1.py
@@ -7,6 +7,15 @@ print('tuple_1 + tuple_2 = ', tuple_1 + tuple_2)
 print('tuple_1 * 3 = ', tuple_1 * 3)  # 반복
 print('tuple_1[0] = ', tuple_1[0]) # 인덱싱
 print('tuple_1[0:3] = ', tuple_1[1:4]) # 슬라이싱 1부터 4전까지
+print('튜플의 개수는 =', len(tuple_1))
+print("마지막 튜플의 값 =", tuple_1[len(tuple_1)-1])
+# 튜플의 데이터 중에서 마지막 3개를 출력 
+print("마지막 튜플의 값= ", tuple_1 [-3:]) #:하고 비워두면 끝까지
+# 튜플의 데이터 중에서 처음 3개를 출력
+print("처음 5개의 값= ", tuple_1 [:5])
+
+
+
 # tuple. append(10) # error
 # tuple은 수정이 불가하다
 
